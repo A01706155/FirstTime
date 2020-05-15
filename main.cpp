89a60afbd6dbaf0c21342159eca83eed50cc0903
A01706155 <A01706155@itesm.mx>
2020-05-15 23:18:24 +0000

    Primer programa triángulo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
-int main(int argc, char* argv[]) {
-  cout << "¡¡¡Hola mundo!!!" << endl;
-  return 0;
+
+class triangulo {
+public:
+double lado_a,lado_b,lado_c,a_cuadrada,c_cuadrada,semifinal;
+
+void medidas() {
+cout << "Ingresa la longitud de ambos lados: ";
+cin >> lado_a;
+lado_b = lado_a;
+cout << "Ingresa la longitud de la base: ";
+cin >> lado_c;
+cout <<"\n";
+}
+
+void verificador() {
+if (lado_a > lado_c)
+cout <<"";
+else 
+cout << "¡¡Advertencia!!. El triángulo especificado no existe. \nEl cálculo dará errores. F.\n\n"; 
+}
+
+void perimetro() {
+cout <<"El perímetro del triángulo es de "<<lado_a+lado_b+lado_c<<" unidades \n";
+}
+
+void area() {
+a_cuadrada=pow(lado_a,2);
+c_cuadrada=pow(lado_c,2);
+semifinal=a_cuadrada-(c_cuadrada/4);
+cout <<"El área del triángulo es de "<< sqrt(semifinal) <<" unidades cuadradas";
+}
+
+};
+
+int main()
+{
+triangulo test;
+test.medidas();
+test.verificador();
+test.perimetro();
+test.area();
+return 0;
+
 }
